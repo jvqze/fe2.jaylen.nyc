2dcfee90341188b91aad2fe3c8b7f4cf6c22177f
Jaylen Ruiz <jaylenjruiz@gmail.com>
2024-10-29 23:49:59 -0400

    can remove this now since the userid works in production

--- src/pages/api/getFiles.ts
+++ src/pages/api/getFiles.ts
@@ -12,8 +12,6 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         case "GET":
             try {
                 const { userid } = req.query;
-
-                console.log("Received userid:", userid);
                 if (!userid || typeof userid !== "string") {
                     console.log("no");
                     return res.status(400).json({ message: "Invalid userid parameter." });
